946dcbbecd8f3ad2387a2fc084773fe33403328f
Radmir Kashapov <radmir.kashapov.150700@gmail.com>
2020-03-10 17:52:10 +0300

    Added table_pages function

--- venv/agent.py
+++ venv/agent.py
@@ -1,16 +1,19 @@
 from selenium import webdriver
 import time
+from lxml.html import fromstring
 
 def get_driver():
     agent = webdriver.Chrome(executable_path="./chromedriver")
-
     return agent
 
 def get_source_code(agent, link):
     agent.get(link)
+    return fromstring(agent.page_source)
 
-#def table_pages(source):
- #   pages = source.xpath()
+
+def table_pages(source):
+    pages = source.xpath("//ul/li/a/@href")
+    return list(set(pages))
 
 if __name__ == "__main__":
     driver = get_driver()
@@ -21,6 +24,7 @@ if __name__ == "__main__":
     ]
 
     for url in urls:
-        print(get_source_code(driver, url))
+        source_code = get_source_code(driver, url)
+        print(table_pages(source_code))
 
     driver.close()
