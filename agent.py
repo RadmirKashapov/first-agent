0a7c71574f0b4dfad382f30b18e94b49bce1b3f1
Radmir Kashapov <radmir.kashapov.150700@gmail.com>
2020-03-10 18:08:41 +0300

    Updated get_data to parse some data

--- agent.py
+++ agent.py
@@ -21,9 +21,18 @@ def get_data(agent, link):
     source_code = get_source_code(agent, link)
     pages = table_pages(source_code)
 
+    headers = source_code.xpath("//thead/tr/th/a/text()")
     for page in pages:
         full_link = link + page
         agent.get(full_link)
+        rows = get_source_code(agent, full_link).xpath("//tbody/tr")
+        for row in rows:
+            cells = row.xpath("td/text()")
+            item = dict()
+            if cells[2] == "Operating":
+                for i, cell in enumerate(cells):
+                    item[headers[i]] = cell
+                table_data.append(item)
 
     return table_data
 
@@ -37,5 +46,6 @@ if __name__ == "__main__":
 
     for url in urls:
         data = get_data(driver, url)
+        print(data)
 
     driver.close()
